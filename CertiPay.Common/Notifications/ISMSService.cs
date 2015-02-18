ac108d0104e2a54bd7a397571e3aeb5bdc148974
Matt Wagner <mattgwagner@gmail.com>
2015-02-18 13:37:01 -0500

    Add SMSService implementation using Twilio

--- CertiPay.Common/Notifications/ISMSService.cs
+++ CertiPay.Common/Notifications/ISMSService.cs
@@ -1,53 +1,52 @@
-﻿//using CertiPay.Common.Logging;
-//using CertiPay.Common.Models.Notifications;
-//using System;
-//using System.Threading.Tasks;
-//using Twilio;
+﻿using CertiPay.Common.Logging;
+using System;
+using System.Threading.Tasks;
+using Twilio;
 
-//namespace CertiPay.Common.Notifications
-//{
-//    /// <summary>
-//    /// Send an SMS message to the given recipient.
-//    /// </summary>
-//    /// <remarks>
-//    /// Implementation may be sent into background processing.
-//    /// </remarks>
-//    public interface ISMSService : INotificationSender<SMSNotification>
-//    {
-//        // Task SendAsync(T notification);
-//    }
+namespace CertiPay.Common.Notifications
+{
+    /// <summary>
+    /// Send an SMS message to the given recipient.
+    /// </summary>
+    /// <remarks>
+    /// Implementation may be sent into background processing.
+    /// </remarks>
+    public interface ISMSService : INotificationSender<SMSNotification>
+    {
+        // Task SendAsync(T notification);
+    }
 
-//    public class SmsService : ISMSService
-//    {
-//        private static readonly ILog Log = LogManager.GetLogger<ISMSService>();
+    public class SmsService : ISMSService
+    {
+        private static readonly ILog Log = LogManager.GetLogger<ISMSService>();
 
-//        private readonly String _twilioAccountSId;
+        private readonly String _twilioAccountSId;
 
-//        private readonly String _twilioAuthToken;
+        private readonly String _twilioAuthToken;
 
-//        private readonly String _twilioSourceNumber;
+        private readonly String _twilioSourceNumber;
 
-//        public SmsService(String twilioAccountSid, String twilioAuthToken, String twilioSourceNumber)
-//        {
-//            this._twilioAccountSId = twilioAccountSid;
-//            this._twilioAuthToken = twilioAuthToken;
-//            this._twilioSourceNumber = twilioSourceNumber;
-//        }
+        public SmsService(String twilioAccountSid, String twilioAuthToken, String twilioSourceNumber)
+        {
+            this._twilioAccountSId = twilioAccountSid;
+            this._twilioAuthToken = twilioAuthToken;
+            this._twilioSourceNumber = twilioSourceNumber;
+        }
 
-//        public Task SendAsync(SMSNotification notification)
-//        {
-//            // TODO Add logging
+        public Task SendAsync(SMSNotification notification)
+        {
+            // TODO Add logging
 
-//            // TODO Add error handling
+            // TODO Add error handling
 
-//            var client = new TwilioRestClient(_twilioAccountSId, _twilioAuthToken);
+            var client = new TwilioRestClient(_twilioAccountSId, _twilioAuthToken);
 
-//            foreach (var recipient in notification.Recipients)
-//            {
-//                client.SendSmsMessage(_twilioSourceNumber, recipient, notification.Content);
-//            }
+            foreach (var recipient in notification.Recipients)
+            {
+                client.SendSmsMessage(_twilioSourceNumber, recipient, notification.Content);
+            }
 
-//            return Task.FromResult(0);
-//        }
-//    }
-//}
+            return Task.FromResult(0);
+        }
+    }
+}
