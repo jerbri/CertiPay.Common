afda66311be272e4aeda35d7d0d97299f06c0140
Matt Wagner <mattgwagner@gmail.com>
2015-02-18 12:46:10 -0500

    Fixed path for package

--- .build/default.ps1
+++ .build/default.ps1
@@ -41,7 +41,7 @@ Task Build -depends Restore-Packages, Update-AssemblyInfoFiles {
 }
 
 Task Package -depends Build {
-	exec { . $NuGet pack CertiPay.Common\CertiPay.Common.nuspec -Properties Configuration=$Configuration -OutputDirectory "$SolutionRoot" -Version "$Version" }
+	exec { . $NuGet pack "$SolutionRoot\CertiPay.Common\CertiPay.Common.nuspec" -Properties Configuration=$Configuration -OutputDirectory "$SolutionRoot" -Version "$Version" }
 }
 
 Task Clean {
