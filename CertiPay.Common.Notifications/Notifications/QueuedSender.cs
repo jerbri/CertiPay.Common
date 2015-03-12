575b4ad81691588df3e2b83278bfc52d48a6b2aa
Matt Wagner <mattgwagner@gmail.com>
2015-03-12 10:48:18 -0400

    Make QueuedSender a partial class so we can extend it with Identity later on

--- CertiPay.Common.Notifications/Notifications/QueuedSender.cs
+++ CertiPay.Common.Notifications/Notifications/QueuedSender.cs
@@ -7,8 +7,7 @@ namespace CertiPay.Common.Notifications
     /// <summary>
     /// Sends notifications to the background worker queue for async processing and retries
     /// </summary>
-    public class QueuedSender :
-        //IIdentityMessageService,
+    public partial class QueuedSender :
         INotificationSender<SMSNotification>,
         INotificationSender<EmailNotification>
     {
@@ -34,37 +33,5 @@ namespace CertiPay.Common.Notifications
 
             await _queue.Enqueue(SMSNotification.QueueName, notification);
         }
-
-        //public async Task SendAsync(IdentityMessage message)
-        //{
-        //    // TODO Add logging
-
-        //    // TODO Might need a sanity check in here to make sure something doesn't go to the wrong format
-
-        //    if (String.IsNullOrWhiteSpace(message.Subject))
-        //    {
-        //        await SendAsync(new SMSNotification
-        //        {
-        //            Recipients = new[]
-        //            {
-        //                message.Destination
-        //            },
-        //            Content = message.Body
-        //        });
-        //    }
-        //    else
-        //    {
-        //        await SendAsync(new EmailNotification
-        //        {
-        //            FromAddress = EmailService.NO_REPLY_ADDR,
-        //            Recipients = new[]
-        //            {
-        //                message.Destination
-        //            },
-        //            Subject = message.Subject,
-        //            Content = message.Body
-        //        });
-        //    }
-        //}
     }
 }
