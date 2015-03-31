c34f0c2b1fb292d21f4ba5428430666a73dbb8d5
Matt Wagner <mattgwagner@gmail.com>
2015-03-31 13:44:51 -0400

    Adding some lame tests for making sure the log manager works

---
+++ CertiPay.Common.Tests/Logging/LogManagerTests.cs
@@ -0,0 +1,14 @@
+﻿using CertiPay.Common.Logging;
+using NUnit.Framework;
+
+namespace CertiPay.Common.Tests.Logging
+{
+    public class LogManagerTests
+    {
+        [Test]
+        public void Ensure_Can_Write_To_Rolling_File()
+        {
+            LogManager.GetCurrentClassLogger().Info("This is some basic text output!");
+        }
+    }
+}
