3cd49339e61159bf1e41d246d9e9bd73b3452538
jerbri <jworld51@hotmail.com>
2015-09-02 20:37:35 -0400

    Added test case for AllowedTestingDomainsEnabled

--- CertiPay.Common.Notifications.Tests/Notifications/EmailServiceTests.cs
+++ CertiPay.Common.Notifications.Tests/Notifications/EmailServiceTests.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using System;
 using System.Net.Mail;
+using CertiPay.Common;
 
 namespace CertiPay.Services.Notifications
 {
@@ -14,6 +15,7 @@ namespace CertiPay.Services.Notifications
         [ExpectedException(ExpectedException = typeof(InvalidOperationException), ExpectedMessage = "A recipient must be specified.")]
         public void Should_Not_Email_Outsiders_NonProd(String email)
         {
+            
             IEmailService emailer = new EmailService(new System.Net.Mail.SmtpClient());
 
             using (var msg = new MailMessage { From = new MailAddress("test@test.com") })
@@ -23,5 +25,47 @@ namespace CertiPay.Services.Notifications
                 emailer.Send(msg);
             }
         }
+
+        [TestCase("mattgwagner@gmail.com")]
+        [TestCase("jsmith@worksafepays.com")]
+        [TestCase("jsmith@certipay.com")]
+        [TestCase("jeremy@jeremyandchana.com")]        
+        public void Should_Allow_Any_Email_When_Testing_Domains_Disabled(string email)
+        {
+
+            EmailService.AllowedTestingDomainsEnabled = true;
+
+            IEmailService emailer = new EmailService(new SmtpClient());
+
+            using (var msg = new MailMessage { From = new MailAddress("test@test.com") })
+            {
+                msg.To.Add(email);
+
+                emailer.Send(msg);
+            }
+
+        }
+
+        [TestCase("mattgwagner@gmail.com")]
+        [TestCase("jsmith@worksafepays.com")]
+        [TestCase("jsmith@certipay.com")]
+        [TestCase("jeremy@jeremyandchana.com")]
+        [ExpectedException(ExpectedException = typeof(InvalidOperationException), ExpectedMessage = "A recipient must be specified.")]
+        public void Should_Not_Allow_Any_Email_When_Testing_Domains_Enabled(string email)
+        {
+
+            EmailService.AllowedTestingDomainsEnabled = false;
+
+            IEmailService emailer = new EmailService(new SmtpClient());
+
+            using (var msg = new MailMessage { From = new MailAddress("test@test.com") })
+            {
+                msg.To.Add(email);
+
+                emailer.Send(msg);
+            }
+
+        }
+
     }
 }
