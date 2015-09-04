51a63210ca1c927e1343178ca903e6231823f10e
Matt Wagner <mattgwagner@gmail.com>
2015-09-04 10:57:00 -0400

    Use StringEnumConvert for Approvals.VerifyJson / VerifyMe
    Note, this is a breaking change if there are existing results with
    enums in their output

--- CertiPay.Common.Testing/TestExtensions.cs
+++ CertiPay.Common.Testing/TestExtensions.cs
@@ -2,18 +2,29 @@
 {
     using ApprovalTests;
     using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
     using Ploeh.AutoFixture;
 
     public static class TestExtensions
     {
         private static readonly Fixture _fixture = new Fixture { };
 
+        private static readonly JsonSerializerSettings _settings = new JsonSerializerSettings
+        {
+            Formatting = Formatting.Indented
+        };
+
+        static TestExtensions()
+        {
+            _settings.Converters.Add(new StringEnumConverter { });
+        }
+
         /// <summary>
         /// Runs ApprovalTests's VerifyJson against a JSON.net serialized representation of the provided object
         /// </summary>
         public static void VerifyMe(this object obj)
         {
-            var json = JsonConvert.SerializeObject(obj);
+            var json = JsonConvert.SerializeObject(obj, _settings);
 
             Approvals.VerifyJson(json);
         }
@@ -21,7 +32,7 @@
         /// <summary>
         /// Returns an auto-initialized instance of the type T, filled via mock
         /// data via AutoFixture.
-        /// 
+        ///
         /// This will not work for interfaces, only concrete types.
         /// </summary>
         public static T AutoGenerate<T>()
