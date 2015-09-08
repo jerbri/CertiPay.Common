ebae744db9daa1862b4d5e9ca2bb446b3d41c127
Matt Wagner <mattgwagner@gmail.com>
2015-09-08 10:52:22 -0400

    Add a few test cases for the Log.Timer signature change

---
+++ CertiPay.Common.Tests/Logging/MetricLoggingExtensionsTests.cs
@@ -0,0 +1,48 @@
+﻿using CertiPay.Common.Logging;
+using NUnit.Framework;
+using System;
+using System.Threading;
+
+namespace CertiPay.Common.Tests.Logging
+{
+    public class MetricLoggingExtensionsTests
+    {
+        private static readonly ILog Log = LogManager.GetLogger<MetricLoggingExtensionsTests>();
+
+        [Test]
+        public void Use_Log_Timer_No_Identifier()
+        {
+            using (Log.Timer("Use_Log_Timer"))
+            {
+                // Cool stuff happens here
+            }
+        }
+
+        [Test]
+        public void Use_Log_Timer_With_Debug()
+        {
+            using (Log.Timer("Use_Log_Timer_With_Debug", level: LogLevel.Debug))
+            {
+                // Debug tracking happens here
+            }
+        }
+
+        [Test]
+        public void Takes_Longer_Than_Threshold()
+        {
+            using (Log.Timer("Takes_Longer_Than_Threshold", warnIfExceeds: TimeSpan.FromMilliseconds(100)))
+            {
+                Thread.Sleep(TimeSpan.FromMilliseconds(150));
+            }
+        }
+
+        [Test]
+        public void Object_Context_Provided()
+        {
+            using (Log.Timer("Object_Context_Provided", new { id = 10, userId = 12 }))
+            {
+                // Cool stuff happens here
+            }
+        }
+    }
+}
