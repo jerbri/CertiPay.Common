c75d6e268ac919cc55c78aace87e0e55f7d34aa3
Matt Wagner <mattgwagner@gmail.com>
2015-09-10 09:45:47 -0400

    Add comments in emailnotification and seal NoOpNotificationSender

--- CertiPay.Common.Notifications/Notifications/EmailNotification.cs
+++ CertiPay.Common.Notifications/Notifications/EmailNotification.cs
@@ -10,14 +10,29 @@ namespace CertiPay.Common.Notifications
     {
         public static String QueueName { get { return "EmailNotifications"; } }
 
+        /// <summary>
+        /// Who the email notification should be FROM
+        /// </summary>
         public String FromAddress { get; set; }
 
+        /// <summary>
+        /// A list of email addresses to CC
+        /// </summary>
         public ICollection<String> CC { get; set; }
 
+        /// <summary>
+        /// A list of email addresses to BCC
+        /// </summary>
         public ICollection<String> BCC { get; set; }
 
+        /// <summary>
+        /// The subject line of the email
+        /// </summary>
         public String Subject { get; set; }
 
+        /// <summary>
+        /// Any attachments to the email in the form of URLs to download
+        /// </summary>
         public ICollection<Attachment> Attachments { get; set; }
 
         public EmailNotification()
@@ -28,6 +43,10 @@ namespace CertiPay.Common.Notifications
             this.BCC = new List<String>();
         }
 
+        /// <summary>
+        /// A file may be attached to the email notification by providing a URL to download
+        /// the file (will be downloaded by the sending process) and a filename
+        /// </summary>
         public class Attachment
         {
             public String Filename { get; set; }
--- CertiPay.Common.Notifications/Notifications/NoOpNotificationSender.cs
+++ CertiPay.Common.Notifications/Notifications/NoOpNotificationSender.cs
@@ -8,7 +8,7 @@ namespace CertiPay.Common.Notifications.Notifications
     /// that just logs the full notification out to the ILog implementation for debugging.
     /// This is useful when running locally, when we don't want actual notifications to get sent out.
     /// </summary>
-    public class NoOpNotificationSender : INotificationSender<EmailNotification>, INotificationSender<SMSNotification>
+    public sealed class NoOpNotificationSender : INotificationSender<EmailNotification>, INotificationSender<SMSNotification>
     {
         private static readonly ILog Log = LogManager.GetCurrentClassLogger();
 
